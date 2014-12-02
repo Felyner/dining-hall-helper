4e3782e7f49ee21b73571af89e4b9c73a1154a44
Dylan Woodbury <dwoodbur@ucsc.edu>
2014-12-02 04:24:04 -0800

    Include functions to display snaps

--- controllers/default.py
+++ controllers/default.py
@@ -7,3 +7,27 @@ def home():
 
 def user():
     return dict(form = auth())
+
+# I use this to download uploaded images.
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+# Gets the snaps of a specific dining hall.
+#    Edit to include only recent ones once done testing.
+def getSnaps():
+    query = (db.snap.DH == request.args[0])
+    snaps = db(query).select(db.snap.ALL, orderby=~db.snap.helpful)
+    response.view = 'default/snaps.html'
+    return dict(snaps = snaps)
+    
+# We should use AJAX to increase "helpful", I don't call this function anymore
+def help():
+    query = (db.snap.picture == request.args[0])
+    snaps = db(query).select(db.snap.ALL)
+    newHelpful = snaps[0].helpful+1
+    db(query).update(helpful = newHelpful)
